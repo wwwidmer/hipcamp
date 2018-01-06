513c4cc3f8956d05603a7d5f6d9d12c84dcfc799
Billy <williamwidmerr@gmail.com>
2018-01-06 15:08:57 -0800

    set up scss and babel, now were ready to code

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,51 @@
 const path = require('path');
 
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
+
 module.exports = {
-  entry: './src/index.js',
-  output:  {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: './src/index.js',
+    output:  {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['react', 'es2015']
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.scss$/,
+          use:  extractSass.extract( {
+            use: [
+              'css-loader',
+              'sass-loader'
+            ],
+            fallback: 'style-loader'
+          })
+        }
+      ]
+    },
+    plugins: [
+      extractSass
+    ],
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
 }
